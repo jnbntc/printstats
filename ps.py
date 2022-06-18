0558c198ac8130c9fd7b6733369a3135ea0e127c
jnbntc <jbentancour@gmail.com>
2022-06-17 22:20:43 -0300

    Change hosts

--- ps.py
+++ ps.py
@@ -2,14 +2,14 @@ from pysnmp import hlapi
 from . import quicksnmp
 
 # Using SNMPv2c, we set the hostname of the remote device to 'SNMPHost'
-quicksnmp.set('10.0.0.1', {'1.3.6.1.2.1.1.5.0': 'SNMPHost'}, hlapi.CommunityData('ICTSHORE'))
+quicksnmp.set('192.168.3.25', {'1.3.6.1.2.1.1.5.0': 'SNMPHost'}, hlapi.CommunityData('ICTSHORE'))
 
 # Using SNMPv2c, we retrieve the hostname of the remote device
-print(get('10.0.0.1', ['1.3.6.1.2.1.1.5.0'], hlapi.CommunityData('ICTSHORE')))
+print(get('192.168.3.25', ['1.3.6.1.2.1.1.5.0'], hlapi.CommunityData('ICTSHORE')))
 
 # We get interface name and Cisco interface description for all interfaces
 # The last parameter is the OID containing the number of interfaces, so we can loop 'em all!
-its = get_bulk_auto('10.0.0.1', [
+its = get_bulk_auto('192.168.3.25', [
     '1.3.6.1.2.1.2.2.1.2 ',
     '1.3.6.1.2.1.31.1.1.1.18'
     ], hlapi.CommunityData('ICTSHORE'), '1.3.6.1.2.1.2.1.0')
