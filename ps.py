bd336f9fbf8c2a4a14701c3eac218c06a7ec36de
Juan <jbentancour@gmail.com>
2022-06-21 17:19:13 +0000

    main

--- ps.py
+++ ps.py
@@ -1,14 +1,22 @@
 from global_setup import *
+import pymysql
 import sys
+from datetime import datetime
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
-def __init__ (self):
-    self.conn = pymysql.connect(MY_DB_SERVER, MY_DB_USER, MY_DB_PASS, MY_DB_DB, use_unicode=True, charset="utf8")
+#def __init__ (self):
+conn = pymysql.connect(host=MY_DB_SERVER, user=MY_DB_USER, password=MY_DB_PASS, db=MY_DB_DB, use_unicode=True, charset="utf8")
 
 SYSNAME = '1.3.6.1.2.1.43.10.2.1.4.1.1'
 
-host = '192.168.3.26'
+host = '192.168.3.28'
 snmp_ro_comm = 'public'
+
+# Getting the current date and time
+dt = datetime.now()
+
+# getting the timestamp
+ts = datetime.timestamp(dt)
 
 # Define a PySNMP CommunityData object named auth, by providing the SNMP community string
 auth = cmdgen.CommunityData(snmp_ro_comm)
@@ -33,9 +41,27 @@ if errorIndication:
 # that we need to iterate over. It provides the OID and the value, both of which have a
 # prettyPrint() method so that you can get the actual string data
 for oid, val in varBinds:
-    print(oid.prettyPrint(), val.prettyPrint())
+    print(val.prettyPrint())
+#    print(oid.prettyPrint(), val.prettyPrint())
 
 
-# Prueba modulo de configuracíon
 
-print(MY_DB_DB)
+with conn.cursor() as cursor:
+	consulta = "INSERT INTO historial(contador, fecha) VALUES (%s, %s);"
+	#Podemos llamar muchas veces a .execute con datos distintos
+	cursor.execute(consulta, (val.prettyPrint(), dt))
+	conn.commit()
+
+
+try:
+	with conn.cursor() as cursor:
+		# En este caso no necesitamos limpiar ningún dato
+		cursor.execute("SELECT id, modelo, ip, ubicacion, ultima_lectura FROM equipos;")
+		# Con fetchall traemos todas las filas
+		equipos = cursor.fetchall()
+
+		# Recorrer e imprimir
+		for equipo in equipos:
+			print(equipo)
+finally:
+	conn.close()
